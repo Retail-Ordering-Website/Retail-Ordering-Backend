64f91dfe048a07811fb5536c2ebc5de13f4a565c
Munirathinam_E <munirathinam6724@gmail.com>
2026-04-24 11:28:21 +0530

    Order and Inventory Related features are added

--- Interfaces/IOrderService.cs
+++ Interfaces/IOrderService.cs
@@ -1,6 +1,13 @@
-﻿namespace RetailOrdering.API.Interfaces
+﻿using RetailOrdering.API.DTOs.Order;
+using RetailOrdering.API.Models;
+
+namespace RetailOrdering.API.Interfaces
 {
-    public class IOrderService
+    public interface IOrderService
     {
+        Task<OrderConfirmationDto> PlaceOrderAsync(int userId, OrderRequestDto dto);
+        Task<List<OrderDto>> GetUserOrdersAsync(int userId);
+        Task<List<OrderDto>> GetAllOrdersAsync();
+        Task UpdateStatusAsync(int orderId, OrderStatus status);
     }
 }
